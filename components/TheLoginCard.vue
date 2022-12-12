dc9235ed8ecf185876c2ce8a0308cc87f509f7c0
Robert Kamrowski <r.kamrowski@cinkciarz.pl>
2022-12-12 08:32:37 +0100

    Add register modal

--- components/TheLoginCard.vue
+++ components/TheLoginCard.vue
@@ -4,9 +4,12 @@
       <h1 class="text-h3 font-weight-bold">Logowanie</h1>
     </VCardTitle>
     <VCardText>
-      <VForm id="login-form" @submit.prevent="$emit('onLogin', credentials)">
+      <VForm
+        id="login-form"
+        @submit.prevent="$emit('onLogin', loginCredentials)"
+      >
         <VTextField
-          v-model="credentials.username"
+          v-model="loginCredentials.username"
           label="Login"
           append-icon="mdi-login"
           hint="Wpisz login"
@@ -15,7 +18,7 @@
           rounded
         />
         <VTextField
-          v-model="credentials.password"
+          v-model="loginCredentials.password"
           type="password"
           label="Hasło"
           append-icon="mdi-key"
@@ -30,7 +33,48 @@
     <VCardActions>
       <VRow>
         <VCol sm="6">
-          <VBtn block outlined> Utwórz konto</VBtn>
+          <VDialog :max-width="480">
+            <template #activator="{ on, attrs }">
+              <VBtn block outlined v-bind="attrs" v-on="on"> Utwórz konto</VBtn>
+            </template>
+            <VCard>
+              <VCardTitle class="mb-4">
+                <h2 class="text-h4 font-weight-bold">Nowe konto</h2>
+              </VCardTitle>
+              <VCardText>
+                <VForm
+                  id="register-form"
+                  @submit.prevent="$emit('onRegister', registerCredentials)"
+                >
+                  <VTextField
+                    v-model="registerCredentials.username"
+                    label="Login"
+                    append-icon="mdi-login"
+                    hint="Wpisz login"
+                    outlined
+                    dense
+                    rounded
+                  />
+                  <VTextField
+                    v-model="registerCredentials.password"
+                    type="password"
+                    label="Hasło"
+                    append-icon="mdi-key"
+                    hint="Wpisz hasło"
+                    outlined
+                    rounded
+                    dense
+                    clearable
+                  />
+                </VForm>
+              </VCardText>
+              <VCardActions>
+                <VBtn form="register-form" type="submit" block>
+                  Utwórz konto
+                </VBtn>
+              </VCardActions>
+            </VCard>
+          </VDialog>
         </VCol>
         <VCol sm="6">
           <VBtn form="login-form" type="submit" block> Zaloguj</VBtn>
@@ -45,7 +89,11 @@ export default {
   name: 'TheLoginCard',
   data() {
     return {
-      credentials: {
+      registerCredentials: {
+        username: '',
+        password: ''
+      },
+      loginCredentials: {
         username: '',
         password: ''
       }
--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,8 @@
 <template>
   <VContainer class="fill-height">
     <VRow justify="center" align="center">
-      <VCol cols="10" sm="8" md="6" lg="5">
-        <TheLoginCard @onLogin="handleLogin" />
+      <VCol cols="10" sm="8" md="6" lg="5" xl="3">
+        <TheLoginCard @onLogin="handleLogin" @onRegister="handleRegister" />
       </VCol>
     </VRow>
   </VContainer>
@@ -15,9 +15,21 @@ export default {
   name: 'LoginPage',
   methods: {
     ...mapMutations(['manageAlert']),
+    // Login user to the app
     async handleLogin(data) {
       try {
         await this.$auth.loginWith('local', { data })
+      } catch ({ response }) {
+        this.manageAlert({
+          show: true,
+          type: 'error',
+          text: response.data.message
+        })
+      }
+    },
+    async handleRegister(data) {
+      try {
+        await this.$axios.$post('/auth/register', data)
       } catch ({ response }) {
         this.manageAlert({
           show: true,
